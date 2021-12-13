909c9a7daeda34df98039578cf439017bc9b8df9
Paweł Sadowski <pawel73@gmail.com>
2021-12-13 19:27:10 +0100

    finddocs

--- finddocs.sh
+++ finddocs.sh
@@ -1,4 +1,4 @@
 rm files-doc.lst
 rm files-pdf.lst
-find /volume1/ -type f -iname "\.doc" >>files-doc.lst
-find /volume1/ -type f -iname "\.pdf" >>files-pdf.lst
+find /volume1/ -type f -iname '*.doc' >>files-doc.lst
+find /volume1/ -type f -iname '*.pdf' >>files-pdf.lst
